e33b33e2026fe667acf9df494181d1a0a35cc7fa
AT514 <ajit_tripathi@yahoo.com>
2020-05-12 12:05:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jyrmp8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jyrmp8)
---
+++ src/app/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'shopping-list',
+  template: `
+    <h1>Shopping List</h1>
+    <input  />
+    <button >Add</button>
+  `,
+  styles: [``]
+})
+export class ShoppingListComponent  {
+
+}
